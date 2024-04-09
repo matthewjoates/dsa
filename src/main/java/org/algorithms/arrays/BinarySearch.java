b101b7fc8ab4e653fd65f511fcd1dd61879e0c8e
matthewjoates <GHBuggers2>
2024-04-09 15:28:57 +0100

    print details of search

--- src/main/java/org/algorithms/arrays/BinarySearch.java
+++ src/main/java/org/algorithms/arrays/BinarySearch.java
@@ -11,18 +11,20 @@ public class BinarySearch {
 
         while (low < high) {
             int midpoint = low + ((high - low) / 2);
-            System.out.println("\nlow: "+low + "; high: "+high + "; midpoint: "+midpoint);
+            System.out.println("low: "+low + "; high: "+high + "; midpoint: "+midpoint);
             double value =  haystack[midpoint];
             if (value == needle) {
                 System.out.println("value found: "+value + " at index: "+midpoint);
                 return midpoint;
             } else if (value > needle){
+                System.out.print("shifting left of array -> ");
                 System.out.print("value > needle: "+value + "; needle: "+needle + ". ");
-                System.out.print("high changing from: "+high + " to : "+midpoint + "\n");
+                System.out.print("high changing from: "+high + " to : "+midpoint + "\n\n");
                 high = midpoint;
             } else {
+                System.out.print("shifting right of array -> ");
                 System.out.print("value < needle: "+value + "; needle: "+needle + ". ");
-                System.out.print("low changing from "+low + " to "+ midpoint +"+1. \n ");
+                System.out.print("low changing from "+low + " to "+ midpoint +"+1. \n\n");
                 // Don't need to consider midpoint because we know it's not the correct value.
                 // Adjust low end and put it right in front of where the midpoint is.
                 low = midpoint + 1;
